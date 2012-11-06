d815983a9111e355b2e415f42ae3797db1db9a72
EricMareth <EricMareth@fullsail.edu>
2012-11-06 00:08:43 -0500

    Added JSON data as well as flight list.

--- Mareth_Eric_assignment3.js
+++ Mareth_Eric_assignment3.js
@@ -1,4 +1,34 @@
-var automatonA = "Tobor";
-var automatonB = "Robbie";
-var gettingRusty = true;
-var needsOil = false;
+// Eric Mareth
+// SDI1211
+// Project 3
+// Story with flowchart construction
+
+// Global Variables
+var droidA = "Tobor";
+var droidB = "Robbie";
+var gettingRusty;
+var needsOil;
+
+var flightList = {
+	destination : 	[
+						"Altair IV",
+						"DuMont",
+						"Tatooine"
+					],
+	dockingBay 	:	[	
+						57,	
+						"X9",
+						94
+					],
+	departure 	:	[
+						"19:56",
+						"19:49",
+						"20:17"
+					],
+	cargo 		:	[
+						"plastic educator",
+						"decoder rings",
+						["Luke", "Ben", "R2"]
+					],
+};
+
