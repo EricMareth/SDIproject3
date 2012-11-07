655632ef2451f7036081eb25c76881715d95e6ad
EricMareth <EricMareth@fullsail.edu>
2012-11-07 02:15:47 -0500

    Adding what I have so far...

--- Mareth_Eric_assignment3.js
+++ Mareth_Eric_assignment3.js
@@ -4,29 +4,55 @@
 // Story with flowchart construction
 
 // Global Variables
-var droidA = "Robbie";
-var droidB = "Tobor";
-var gettingRusty;
-var needsOil;
+var currentTime = 43;
+var droid;
+var listIndex;
+var oil;
 
-// FlightList Object
-var flightList = {
-	upc	: 	{
-				destination	: 	"Altair IV",
-				dockingBay	:	57,
-				departure	:	"19:56",
-				cargo		:	"plastic educator"
-			},
-	gII :	{
-				destination	:	"DuMont",
-				dockingBay	:	"X9",
-				departure	:	"19:49",
-				cargo		:	"decoder rings"
-			},
-	mF 	:	{
-				destination	:	"Tatooine",
-				dockingBay	:	94,
-				departure	:	"20:17",
-				cargo		:	["Chewbacca", "Luke", "Ben", "R2"]
-			}
+var geT = {
+
+	minutes : function(){					// *While Loop, *Method Function,
+		while (currentTime < 49){ 			// *Argument Number, *Return Number
+			currentTime++;
+			var newTime = currentTime;
+			return newTime
+		};
+	},
+
+	timeStamp : function(){					
+		console.log("\"Attention spaceport travelers: The time is now 19:" + geT.minutes() + ".\"");
+	},
+	
+	name : function(number){				// *Method: Accessor
+		var droid = shipList.stats[number].droid;
+		return droid
+	},
+	
+	cargo : function(number2){
+		var list = shipList.stats[number2].cargo;
+		return list
+	}
 };
+
+var speak = {
+	needOil	:"\"My rust levels are high, I am going to need oil soon.\"",
+	noOil	:"\"I got my 30,000 mile checkup last week, so my rust levels are low.\"",
+	topic1	:"\"I am hauling" + geT.cargo(1) + ".\" said " + geT.name(1) + ".",
+	topic2	:"\"All we have is " + geT.cargo(0) + ".\" said " + geT.name(0) + ".",
+};
+
+
+geT.timeStamp();
+
+console.log(geT.name(0) + " meets up with " + geT.name(1) +" in a remote spaceport.");
+console.log(geT.name(1) + " says " + speak.needOil + ".");
+console.log(geT.name(0) + " says " + speak.noOil + ".");
+
+geT.timeStamp();
+
+console.log(speak.topic1);
+console.log(speak.topic2);
+
+geT.timeStamp();
+geT.timeStamp();
+geT.timeStamp();
